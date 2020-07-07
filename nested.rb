c27f4e35d8a084b719224c990c3089210e3454f9
Victor Alberto Amigo <amigo.victor.97@gmail.com>
2020-07-07 23:49:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,9 +81,11 @@ def adding_matz
         :languages => ["C"]
       }
     }
-    programmer_hash[:yukihiro_matsumoto] = {
-      :known_for => "Ruby"
+    programmer_hash = {
+    :yukihiro_matsumoto] => {
+      :known_for => "Ruby" ,
       :languages => ["LISP", "C"]
+      }
     }
   return programmer_hash
 end
