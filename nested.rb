5f42fd2b5def7867b3510dbd4369bac39c6cea33
Victor Alberto Amigo <amigo.victor.97@gmail.com>
2020-07-07 23:41:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,7 +107,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+  return programmer_hash
 end
 
 def adding_to_dennis
@@ -129,5 +130,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+  programmer_hash
 end
