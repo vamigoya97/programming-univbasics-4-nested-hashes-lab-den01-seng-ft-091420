ccc442f6d9277bb2c5416d1272ebb05ea9aea928
Victor Alberto Amigo <amigo.victor.97@gmail.com>
2020-07-07 23:51:26 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+  return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+  return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+  return programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -81,7 +81,13 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+    programmer_hash = {
+    :yukihiro_matsumoto => {
+      :known_for => "Ruby" ,
+      :languages => ["LISP", "C"]
+      }
+    }
+  return programmer_hash
 end
 
 def changing_alan
@@ -103,7 +109,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+  return programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +132,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+  programmer_hash
 end
